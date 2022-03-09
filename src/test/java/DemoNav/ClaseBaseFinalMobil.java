45d7c8dc3bb267551f2a1825753767ff968ae055
Ema <eveg7719@gmail.com>
2022-03-09 13:25:41 -0500

    primer comentario de prueba

--- src/test/java/DemoNav/ClaseBaseFinalMobil.java
+++ src/test/java/DemoNav/ClaseBaseFinalMobil.java
@@ -46,6 +46,8 @@ public class ClaseBaseFinalMobil
 		
 	}
 	
+	
+	//esto es un comentario de prueba, para el primer commit
 	   //METODO DE NAVEGADOR
     @SuppressWarnings("rawtypes")
 	public  static AppiumDriver AppiumDriverConnection() {
